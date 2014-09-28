400838942f905b7cd7c6120409353ed8a8adfafb
Reza Fatahi <aug2uag@gmail.com>
2014-09-28 00:27:19 -0700

    client reload data

--- MtrWrkshp.js
+++ MtrWrkshp.js
@@ -3,68 +3,6 @@ var Vids = new Meteor.Collection('videos');
 
 if (Meteor.isClient) {
 
-  var acc = [];
-  
-  HTTP.get("https://www.googleapis.com/youtube/v3/search?key=$KEY&channelId=UC3fBiJrFFMhKlsWM46AsAYw&part=snippet,id&order=date&maxResults=50", function (error, result) {
-    if (!error && result.statusCode === 200) {
-      var total = Math.ceil(JSON.parse(result.content)['pageInfo']['totalResults']/50);
-
-      var n = JSON.parse(result.content)['nextPageToken'] || '';
-      Session.set('nextPage', n);
-
-      acc.push(JSON.parse(result.content)['items']);
-      
-      var i = 1, arr = [];
-      while (i < total) {
-        i++;
-        arr.push(i);
-      }
-
-      var results = [];
-      function series(item) {
-        if(item) {
-          paginate(function(result) {
-            results.push(result);
-            return series(arr.shift());
-          });
-        } else {
-          return final();
-        }
-      }
-      series(arr.shift());
-    }
-  });
-
-  function final() {
-    acc = _.unique(_.flatten(acc));
-    _.forEach(acc, function(itm) {
-      var d = new Date(itm.snippet.publishedAt);
-      if (itm && itm.snippet.title.toLowerCase().indexOf('devshop') > -1 && d.getFullYear() > 2000) {
-        Vids.insert({
-          title: itm.snippet.title,
-          date: (d.getMonth() + 1) + '/' + d.getDate() + '/' +  d.getFullYear(),
-          urlink: itm.id.videoId,
-          rawd: d
-        });
-      } 
-    });
-  }
-
-  function paginate(callback) {
-    var req = "https://www.googleapis.com/youtube/v3/search?key=$KEY&channelId=UC3fBiJrFFMhKlsWM46AsAYw&pageToken=" + Session.get('nextPage') + "&part=snippet,id&order=date&maxResults=50";
-    HTTP.get(req, function (error, result) {
-      if (!error && result.statusCode === 200) {
-        var n = JSON.parse(result.content)['nextPageToken'] || '';
-        Session.set('nextPage', n);
-
-        acc.push(JSON.parse(result.content)['items']);
-
-        callback(0);
-
-  }
-    });
-  }
-
   Meteor.subscribe('vids');
 
   Template.vids.videos = function() {
@@ -80,6 +18,66 @@ if (Meteor.isServer) {
 
     // ensureIndex on the server side
     Vids._ensureIndex({ "title": 1}, {unique:true});
+
+    function paginate(callback) {
+      var req = "https://www.googleapis.com/youtube/v3/search?key=$KEY&channelId=UC3fBiJrFFMhKlsWM46AsAYw&pageToken=" + n + "&part=snippet,id&order=date&maxResults=50";
+      HTTP.get(req, function (error, result) {
+        if (!error && result.statusCode === 200) {
+          n = JSON.parse(result.content)['nextPageToken'] || '';
+
+          acc.push(JSON.parse(result.content)['items']);
+
+          callback(0);
+
+        }
+      });
+    }
+
+    function final() {
+      acc = _.unique(_.flatten(acc));
+      _.forEach(acc, function(itm) {
+        var d = new Date(itm.snippet.publishedAt);
+        if (itm && itm.snippet.title.toLowerCase().indexOf('devshop') > -1 && d.getFullYear() > 2000) {
+          Vids.insert({
+            title: itm.snippet.title,
+            date: (d.getMonth() + 1) + '/' + d.getDate() + '/' +  d.getFullYear(),
+            urlink: itm.id.videoId,
+            rawd: d
+          });
+        } 
+      });
+    }
+
+    var acc = [], n;
+  
+    HTTP.get("https://www.googleapis.com/youtube/v3/search?key=$KEY&channelId=UC3fBiJrFFMhKlsWM46AsAYw&part=snippet,id&order=date&maxResults=50", function (error, result) {
+      if (!error && result.statusCode === 200) {
+        var total = Math.ceil(JSON.parse(result.content)['pageInfo']['totalResults']/50);
+
+        n = JSON.parse(result.content)['nextPageToken'] || '';
+
+        acc.push(JSON.parse(result.content)['items']);
+        
+        var i = 1, arr = [];
+        while (i < total) {
+          i++;
+          arr.push(i);
+        }
+
+        var results = [];
+        function series(item) {
+          if(item) {
+            paginate(function(result) {
+              results.push(result);
+              return series(arr.shift());
+            });
+          } else {
+            return final();
+          }
+        }
+        series(arr.shift());
+      }
+    });
 
   });
 
